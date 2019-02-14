936f7ff090ed1c548ae731d1e788cbda165690cc
Calberg89 <chris_alberg@yahoo.com>
2019-02-13 19:33:06 -0800

    Established files

---
+++ liri.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+var keys = require("./keys.js");
+var spotify = new spotify(keys.spotify);
+
+function concert-this() {
+
+};
+function spotify-this-song() {
+
+};
+function movie-this() {
+
+};
+function do-what-it-says() {
+
+};
